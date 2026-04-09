ef7a1a31c86897f77b6fe217ee65911acc4bb911
Ikram <ikramxiyasov2@gmail.com>
2026-04-09 22:42:40 +0500

    inital commit

--- voice_assistant/settings.py
+++ voice_assistant/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-@h)07v=f01o8b1+l^!q*185%7-le_5b39g+uza!nx27^l--&w%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
